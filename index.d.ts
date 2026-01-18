06d2c491c2d937d8ac79479548d420fbcf76ce81
우원 <aepeul@aepeul-ui-MacBookPro.local>
2026-01-18 16:29:31 +0900

    feat: base type 정의

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 declare module '@env' {
   export const API_URL: string;
   export const API_PREFIX: string;
-  export const NAVER_CONSUMER_ID: string;
-  export const NAVER_CONSUMER_SECRET: string;
-  export const NAVER_APP_NAME: string;
-  export const SERVICE_URL_SCHEME: string;
+  export const IOS_GOOGLE_CLIENT_ID: string;
+  export const IOS_GOOGLE_REDIRECT_URI: string;
+  export const ANDROID_GOOGLE_CLIENT_ID: string;
+  export const ANDROID_GOOGLE_REDIRECT_URI: string;
 }
 
 declare module '*.svg' {
@@ -19,4 +19,98 @@ type BaseType = {
   id: number;
   createdAt?: string;
   updatedAt?: string;
+};
+
+type Chat = {
+  role: 'user' | 'assistant' | 'error';
+  content: string;
+};
+
+type News = {
+  id: number;
+  title: string;
+  content: string;
+  journalist: string;
+  src: string;
+  press: string;
+  date: string;
+  url: string;
+  summary: string;
+};
+
+type SideMessageType = {
+  leftMessage?: React.ReactNode | null;
+  rightMessage?: React.ReactNode | null;
+};
+
+type HistoryElementType = {
+  interviewId: number;
+  userId: number;
+  title: string;
+  totalCnt: number;
+  useCnt: number;
+  status: number;
+  regDate: string;
+};
+
+type QuestionElementType = {
+  interviewId: number;
+  qaId: number;
+  question: string;
+  answer: string;
+  regDate: string;
+  modifyDate: string;
+};
+
+type QuestionType = {
+  role: 'user' | 'interviewer' | 'ai';
+  interviewId: number;
+  qaId: number;
+  question: string;
+  answer: string;
+  regDate: string;
+  modifyDate: string;
+};
+
+type DocumentPDFType = {
+  resumeId: number;
+  fileName: string;
+  regDate: string;
+};
+
+type ResultType = {
+  interviewId: number;
+  interviewerFeel: string;
+  userId: number;
+  title: string;
+  status: number;
+  regDate: string;
+  feedback: string;
+  qaData: {
+    answer: string;
+    bestAnswer: string;
+    interviewId: number;
+    keyword: string[];
+    modifyDate: string;
+    qaId: number;
+    question: string;
+    regDate: string;
+  }[];
+};
+
+type instantPopUpType = {
+  stop: boolean;
+  date: string;
+};
+
+type ArticleType = {
+  id: number;
+  title: string;
+  description: string;
+  link: string;
+  thumbnail: string;
+  published: string;
+  guid: string;
+  companyName: string;
+  createdAt: string;
 };
